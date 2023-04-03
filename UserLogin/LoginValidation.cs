1e1f2686e3365185036bf241a96a23a880793a49
Lin <linkmaslink@abv.bg>
2023-04-03 23:15:23 +0300

    Create new class LoginValidation and add Validation method

---
+++ UserLogin/LoginValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserLogin
+{
+    internal class LoginValidation
+    {
+        public bool ValidateUserInput(string username, string password)
+        {
+            if (username.Length > 4 && password.Length < 6)
+            {
+                Console.WriteLine("UserName: {0}, Password: {1}", username, password);
+                return true;
+            }
+            else
+                Console.WriteLine("Incorrect input!");
+                return false;
+           
+        }
+    }
+}
--- UserLogin/Program.cs
+++ UserLogin/Program.cs
@@ -8,12 +8,15 @@ namespace UserLogin
         static void Main(string[] args)
         {
             User user = new User();
-            user.userName = "LinaT";
+            user.userName = "Li";
             user.password = "0000";
             user.facNumber = "121220100";
             user.role = 0;
 
-            Console.WriteLine("Username: " + user.userName + " " + "Password: " + user.password + " " + "FacNumber: " + user.facNumber + " " + "Role: " + user.role);
+            LoginValidation login = new LoginValidation();
+            login.ValidateUserInput(user.userName, user.password);
+
+            //Console.WriteLine("Username: " + user.userName + " " + "Password: " + user.password + " " + "FacNumber: " + user.facNumber + " " + "Role: " + user.role);
            
         }
     }
